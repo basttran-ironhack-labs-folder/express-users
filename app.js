38480fd6871335b6f5605cb442783b618e6fc049
Bastien Tran <bastien.tran@gmail.com>
2019-02-08 11:18:42 +0100

    about to install passport.js

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const hbs = require("hbs");
 const mongoose = require("mongoose");
 const logger = require("morgan");
 const path = require("path");
+const session = require("express-session");
+const flash = require("connect-flash");
+const MongoStore = require("connect-mongo")(session);
 
 mongoose
   .connect("mongodb://localhost/express-users", { useNewUrlParser: true })
@@ -43,15 +46,47 @@ app.use(
   })
 );
 
+// connect the "views/partials/" folder to the HBS for using partials
+hbs.registerPartials(path.join(__dirname, "views", "partials"));
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
+
+// make our Express app create SESSIONS (more on this tomorrow)
+app.use(
+  session({
+    // saveUninitialized & resave are just to avoid warning messages
+    saveUninitialized: true,
+    resave: true,
+    // should be a string that's different for every app
+    secret: "ca^khT8KYd,G69C7R9(;^atb?h>FTW6664pqEFUKs3",
+    // store our session data inside our MongoDB using "connect-mongo" package
+    store: new MongoStore({ mongooseConnection: mongoose.connection })
+  })
+);
+// allow our routes to use FLASH MESSAGES -- feedback messages before redirects
+// (flash messages need sessions to work)
+app.use(flash());
+//app.use() defines our own MIDDLEWARE function
+app.use((req, res, next) => {
+  // send flash messages to the hbs file
+  // (req.flash() comes from the "connect-flash" npm package)
+  res.locals.messages = req.flash();
+
+  // tell Express we are ready to move to the routes now
+  // (you need this or your pages will stay loading forever)
+  next();
+});
 
 // default value for title local
 app.locals.title = "Express Users";
 
 const index = require("./routes/index");
 app.use("/", index);
+
+const auth = require("./routes/auth-router.js");
+app.use("/", auth);
 
 module.exports = app;
---
+++ routes/auth-router.js
@@ -0,0 +1,82 @@
+const express = require("express");
+const bcrypt = require("bcrypt");
+
+const User = require("../models/user-model.js");
+
+const router = express.Router();
+
+router.get("/signup", (req, res, next) => {
+  res.render("auth-views/signup-form.hbs");
+});
+
+router.post("/process-signup", (req, res, next) => {
+  const { fullName, email, originalPassword } = req.body;
+
+  // enforce password rules (can't be EMPTY and MUST have a digit)
+  if (!originalPassword || !originalPassword.match(/[0-9]/)) {
+    // req.flash() sends a feedback message before a redirect
+    // (it's defined by the "connect-flash" npm package
+    req.flash("error", "Password can't be blank and must contain a number.");
+    // redirect to the SIGNUP PAGE if the password is BAD
+    res.redirect("/signup");
+    return;
+  }
+
+  // encrypt the user's password before saving
+  const encryptedPassword = bcrypt.hashSync(originalPassword, 10);
+  User.create({ fullName, email, encryptedPassword })
+    .then(() => {
+      // req.flash() sends a feedback message before a redirect
+      // (it's defined by the "connect-flash" npm package)
+      req.flash("success", "Sign up success!");
+      // redirect to the HOME PAGE if the sign up worked
+      res.redirect("/");
+    })
+    .catch(err => next(err));
+});
+
+router.get("/login", (req, res, next) => {
+  res.render("auth-views/login-form.hbs");
+});
+
+router.post("/process-login", (req, res, next) => {
+  const { email, originalPassword } = req.body;
+
+  // validate the email by searching the database for an account with that email
+  User.findOne({ email: { $eq: email } })
+    .then(userDoc => {
+      if (!userDoc) {
+        // req.flash() sends a feedback message before a redirect
+        // (it's defined bt the "connect-flash" npm package)
+        req.flash("error", "Email is incorrect.");
+        // redirect to LOGIN PAGE if result is NULL (no account with the email)
+        res.redirect("/login");
+        // use return to STOP the function here if the email is BAD
+        return;
+      }
+
+      // validate the password by using bcrypt.comparSync()
+      const { encryptedPassword } = userDoc;
+
+      if (!bcrypt.compareSync(originalPassword, encryptedPassword)) {
+        req.flash("error", "Password is incorrect.");
+        // redirect to LOGIN PAGE if the passwords don't match
+        res.redirect("/login");
+        // use return to STOP the function here if the PASSWORD is BAD
+        return;
+      }
+      // email & password are CORRECT!
+      // if we MANUALLY managed the user session
+      req.session.userId = userDoc._id;
+
+      // instead we will use PASSPORT - an npm package for managing user sessions
+
+      // req.flash() sends a feedback message before a redirect
+      // (it's defined by the "connect-flash" npm package)
+      req.flash("success", "Log in success!");
+      res.redirect("/");
+    })
+    .catch(err => next(err));
+});
+
+module.exports = router;
